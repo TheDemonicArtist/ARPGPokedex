52d100e91f21deadbaf7bb57e2c550085c99eb03
TheDemonicArtist <72328608+TheDemonicArtist@users.noreply.github.com>
2024-09-17 17:17:14 -0400

    Add files via upload

---
+++ styles/config.js
@@ -0,0 +1,150 @@
+/* ------------------------------------------------------------------- */
+/* Sheet ID
+/* Your sheet ID
+/* ------------------------------------------------------------------- */
+let sheetID = "1EYwH7f7Bj5xpT2NWso3pXglKWapSzELGzGWWPgvQpu4";
+
+
+/* ------------------------------------------------------------------- */
+/* All sheet pages
+/* ------------------------------------------------------------------- */
+let sheetPages = {
+
+    pokedex: "pokedex",
+    masterlistLog: "masterlist log",
+    inventory: "inventory",
+    inventoryLog: "inventory log",
+    items: "items",
+    traits: "traits",
+    prompts: "prompts",
+    faq: "faq",
+    staff: "mods",
+
+}
+
+
+/* ------------------------------------------------------------------- */
+/* All Site Options
+/* ------------------------------------------------------------------- */
+let options = {
+
+
+    /* Index
+    /* --------------------------------------------------------------- */
+    index: {
+
+        promptSheetPage: sheetPages.prompts,
+        numOfPrompts: 3,
+
+        staffSheetPage: sheetPages.staff,
+        numOfStaff: 8,
+
+        pokedexSheetPage: sheetPages.pokedex,
+        numOfDesigns: 4,
+    
+    },
+
+
+    /* Masterlist
+    /* --------------------------------------------------------------- */
+    pokedex: {
+
+        sheetPage: sheetPages.pokedex,
+        logSheetPage: sheetPages.masterlistLog,
+
+        itemAmount: 24,
+        itemOrder: "asc",
+
+        filterColumn: 'Generation',
+        searchFilterParams: ['ID', 'Species', 'Types 1', 'Moves', 'Tags','Colors'],
+        fauxFolderColumn: 'Generation',
+
+    },
+
+
+    /* Item Catalogue
+    /* --------------------------------------------------------------- */
+    items: {
+
+        sheetPage: sheetPages.items,
+    
+        itemAmount: 24,
+        itemOrder: "asc",
+    
+        filterColumn: 'Rarity',
+        searchFilterParams: ['Item'],
+        fauxFolderColumn: 'Type',
+    
+    },
+
+
+    /* Invetory
+    /* --------------------------------------------------------------- */
+    inventory: {
+
+        sheetPage: sheetPages.inventory,
+        itemSheetPage: sheetPages.items,
+        logSheetPage: sheetPages.inventoryLog,
+    
+        itemAmount: 24,
+        sortTypes: ['Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
+        
+        searchFilterParams: ['Username'],
+    
+    },
+
+
+    /* Prompts
+    /* --------------------------------------------------------------- */
+    prompts: {
+    
+        sheetPage: sheetPages.prompts,
+
+        itemAmount: 24,
+        itemOrder: "desc",
+
+        searchFilterParams: ['Title'],
+    
+    },
+
+
+    /* Traits
+    /* --------------------------------------------------------------- */
+    traits: {
+    
+        sheetPage: sheetPages.traits,
+
+        itemAmount: 24,
+        itemOrder: "asc",
+
+        filterColumn: 'Rarity',
+        searchFilterParams: ['Trait'],
+        fauxFolderColumn: 'Type',
+    
+    },
+
+
+    /* Staff
+    /* --------------------------------------------------------------- */
+    staff: {
+    
+        sheetPage: sheetPages.staff,
+    
+    },
+
+
+    /* FAQ
+    /* --------------------------------------------------------------- */
+    faq: {
+    
+        sheetPage: sheetPages.faq,
+    
+        itemAmount: 24,
+        itemOrder: "asc",
+    
+        searchFilterParams: ['Tags'],
+    
+    },
+
+
+}
